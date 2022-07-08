c1d94c0ba191e68911750296b6a994d521df672f
dien-dev <ptdien.developer@gmail.com>
2022-07-08 11:21:35 +0700

    Bug fixes and header improvement #2

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -53,7 +53,7 @@ function Menu({ children, items = [], hideOnClick = false, onchange = defaultFn 
                                 }}
                             />
                         )}
-                        {renderItems()}
+                        <div className={cx('menu-body')}>{renderItems()}</div>
                     </PopperWrapper>
                 </div>
             )}
